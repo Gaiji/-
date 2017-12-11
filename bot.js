614359e9b3c3682d285777f12259a1cd80af3a13
Gaiji <34226052+Gaiji@users.noreply.github.com>
2017-12-11 15:22:11 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setPresence({ game: { name: '真夏の夜の淫夢', type: 1, url: "http://dic.nicovideo.jp/a/真夏の夜の淫夢"} });
+    client.user.setPresence({ game: { name: '真夏の夜の淫夢', type: 1, url: 'http://dic.nicovideo.jp/a/真夏の夜の淫夢' });
 });
 
 client.on('message', message => {
