90e89946f04a19651a3194aabfde330222c20e51
Gaiji <34226052+Gaiji@users.noreply.github.com>
2017-12-11 14:57:26 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,14 +3,14 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setPresence({ game: { name: '真夏の夜の淫夢', type: 4 } });
+    client.user.setPresence({ game: { name: '真夏の夜の淫夢', type: 2 } });
 });
 
 client.on('message', message => {
     if (message.content === "hello"){
         message.reply("hello");
     }
-    if (message.equals("何でもします") || message.equals("なんでもします")) {
+    if (message.content.equals("何でもします")) {
     	message.reply("今何でもするって言ったよね？");
   	}
 });
