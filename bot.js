87c036d22d7ea5374f78699f0cf1b46000484f9d
Gaiji <34226052+Gaiji@users.noreply.github.com>
2017-12-11 15:26:38 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,8 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setPresence({ game: { name: '真夏の夜の淫夢', type: 1, url: 'https;//twitch.tv/truepixels' });
+    client.user.setStatus('Online')
+    client.user.setPresence({ game: { name: '真夏の夜の淫夢', type: 1 });
 });
 
 client.on('message', message => {
