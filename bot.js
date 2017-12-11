96f7ef7f94755270877853b771d9192327b32f92
Gaiji <34226052+Gaiji@users.noreply.github.com>
2017-12-11 14:55:18 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setPresence({ game: { name: '真夏の夜の淫夢', type: 3 } });
+    client.user.setPresence({ game: { name: '真夏の夜の淫夢', type: 4 } });
 });
 
 client.on('message', message => {
